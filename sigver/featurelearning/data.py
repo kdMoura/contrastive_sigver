3a653ffd72ac4e8cf269e1d7c9e6bafb2e6a61a7
Kecia <kecia.de.moura+github@gmail.com>
2024-09-08 21:56:30 -0400

    extract features

--- sigver/featurelearning/data.py
+++ sigver/featurelearning/data.py
@@ -2,6 +2,9 @@ import torch
 from torch.utils.data import Dataset, TensorDataset, DataLoader
 from torchvision import transforms as transforms
 
+from sigver.featurelearning.models.signet import SigNet
+from sigver.datasets.util import load_dataset
+
 
 class TransformDataset(Dataset):
     """
@@ -46,3 +49,31 @@ def extract_features(x, process_function, batch_size, input_size=None):
         for batch in data_loader:
             result.append(process_function(batch))
     return torch.cat(result).cpu().numpy()
+
+def get_features(model_path, data, gpu_idx=0, input_size=(150, 220), batch_size=32):
+    
+    state_dict, class_weights, forg_weights = torch.load(model_path,map_location=lambda storage, loc: storage)
+    device = torch.device('cuda', gpu_idx) if torch.cuda.is_available() else torch.device('cpu')
+
+    print('Using device: {}'.format(device))
+
+    #base_model = models.available_models['signet']().to(device).eval()
+    base_model = SigNet().to(device).eval()
+
+    base_model.load_state_dict(state_dict)
+
+    def process_fn(batch):
+        input = batch[0].to(device)
+        return base_model(input)
+
+    if isinstance(data, str):
+        # if data is a path, load preprocessed data
+        x, y, yforg, user_mapping, filenames = load_dataset(data)
+    
+        features = extract_features(x, process_fn, batch_size, input_size)
+
+        return features, y, yforg, user_mapping, filenames
+    
+    # if not path, extract features from provided data
+    return extract_features(data, process_fn, batch_size, input_size)
+    
