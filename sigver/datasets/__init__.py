5fd6b7f7c39bfc1a067e0c49fd3d91a965d9ca5c
Kecia <kecia.de.moura+github@gmail.com>
2024-09-08 20:28:35 -0400

    add bengali and hindi

--- sigver/datasets/__init__.py
+++ sigver/datasets/__init__.py
@@ -3,10 +3,14 @@ from .gpds_synth import GPDSSynthDataset
 from .mcyt import MCYTDataset
 from .cedar import CedarDataset
 from .brazilian import BrazilianDataset, BrazilianDatasetWithoutSimpleForgeries
+from .bhsig260_bengali import BengaliDataset
+from .bhsig260_hindi import HindiDataset
 
 available_datasets = {'gpds': GPDSDataset,
                       'gpds_synth': GPDSSynthDataset,
                       'mcyt': MCYTDataset,
                       'cedar': CedarDataset,
                       'brazilian': BrazilianDataset,
-                      'brazilian-nosimple': BrazilianDatasetWithoutSimpleForgeries}
+                      'brazilian-nosimple': BrazilianDatasetWithoutSimpleForgeries,
+                      'bengali':BengaliDataset,
+                      'hindi':HindiDataset}
